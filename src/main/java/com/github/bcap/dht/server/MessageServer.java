369454dee88e1ba5d7a3ec3a7231ab52bca007a5
Bruno Polaco Penteado <polaco@gmail.com>
2010-08-27 20:45:11 -0300

    Argument was incorrect

--- src/main/java/com/github/bcap/dht/server/MessageServer.java
+++ src/main/java/com/github/bcap/dht/server/MessageServer.java
@@ -1,10 +1,9 @@
 package com.github.bcap.dht.server;
 
 import com.github.bcap.dht.message.request.Request;
-import com.github.bcap.dht.operation.Operation;
 
 public interface MessageServer {
 	
-	public void send(Request request, Operation owner);
+	public void send(Request request, ResponseHandler handler);
 	
 }
