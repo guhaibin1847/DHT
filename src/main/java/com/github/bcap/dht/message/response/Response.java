629b1a0bcaa95b6189e6a21cddbbd1aa8340e372
Bruno Polaco Penteado <polaco@gmail.com>
2010-08-27 20:00:11 -0300

    Adding status handling

--- src/main/java/com/github/bcap/dht/message/response/Response.java
+++ src/main/java/com/github/bcap/dht/message/response/Response.java
@@ -3,5 +3,23 @@ package com.github.bcap.dht.message.response;
 import com.github.bcap.dht.message.Message;
 
 public abstract class Response extends Message {
+	
+	public enum Status {
+		OK,
+		TIMEOUT
+	}
+	
+	private Status status;
+	
+	public boolean isSuccess() {
+		return this.status == Status.OK;
+	}
 
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
 }
