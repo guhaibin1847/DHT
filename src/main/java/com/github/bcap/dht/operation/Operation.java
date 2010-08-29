0049be3d3fcc340f086556546c56652b01bff3d6
polaco <polaco@polacomac>
2010-08-29 10:10:21 -0300

    Renaming MessageServer to MessageSender as it will not handle requests, only send them and handle responses

--- src/main/java/com/github/bcap/dht/operation/Operation.java
+++ src/main/java/com/github/bcap/dht/operation/Operation.java
@@ -2,22 +2,22 @@ package com.github.bcap.dht.operation;
 
 import com.github.bcap.dht.message.request.Request;
 import com.github.bcap.dht.message.response.Response;
-import com.github.bcap.dht.server.MessageServer;
+import com.github.bcap.dht.server.MessageSender;
 import com.github.bcap.dht.server.ResponseHandler;
 
 public abstract class Operation <Req extends Request, Resp extends Response> implements ResponseHandler<Resp> {
 
-	protected MessageServer messageServer;
+	protected MessageSender messageServer;
 	
 	public void sendRequest(Req request) {
 		messageServer.send(request, this);
 	}
 	
-	public MessageServer getMessageServer() {
+	public MessageSender getMessageServer() {
 		return messageServer;
 	}
 
-	public void setMessageServer(MessageServer server) {
+	public void setMessageServer(MessageSender server) {
 		this.messageServer = server;
 	}
 }
--- src/main/java/com/github/bcap/dht/server/MessageSender.java
+++ src/main/java/com/github/bcap/dht/server/MessageSender.java
@@ -2,7 +2,7 @@ package com.github.bcap.dht.server;
 
 import com.github.bcap.dht.message.request.Request;
 
-public interface MessageServer {
+public interface MessageSender {
 	
 	public void send(Request request, ResponseHandler handler);
 	
