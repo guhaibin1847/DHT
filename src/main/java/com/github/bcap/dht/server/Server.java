5c7f0ad669bc1982f518cec0377f97dc1fb71aa1
Bruno Polaco Penteado <polaco@gmail.com>
2010-08-30 20:15:09 -0300

    - Improved logging - Added constructor parameters for fine control - Added accessor methods

--- src/main/java/com/github/bcap/dht/server/Server.java
+++ src/main/java/com/github/bcap/dht/server/Server.java
@@ -10,6 +10,7 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
@@ -33,15 +34,21 @@ public class Server extends Thread implements Runnable {
 	private static final Logger logger = Logger.getLogger(Server.class);
 
 	public static final int DEFAULT_BACKLOG_SIZE = 20;
+	public static final int DEFAULT_MINIMUM_POOL_SIZE = 1;
+	public static final int DEFAULT_MAXIMUM_POOL_SIZE = 30;
+	public static final long DEFAULT_POLL_THREAD_ALIVE_TIME = 60000;
 
 	private static int SERVER_COUNTER = 0;
 	
 	private Map<Class<? extends Request>, RequestHandler> handlers;
 	private Map<Identifier, Node> nodes;
-	
+
 	private InetAddress ip;
 	private int port;
 	private int backlogSize;
+	private int minimumPoolSize;
+	private int maximumPoolSize;
+	private long poolThreadAliveTime;
 
 	private ServerSocket serverSocket;
 	private ThreadPoolExecutor workerThreadPool;
@@ -49,15 +56,21 @@ public class Server extends Thread implements Runnable {
 	private boolean hasToRun = true;
 	private boolean running = false;
 
-	
-	public Server(InetAddress ip, int port) {
-		this(ip, port, DEFAULT_BACKLOG_SIZE);
+	public Server(int port) throws UnknownHostException {
+		this(Inet4Address.getByName("0.0.0.0"), port);
 	}
 	
-	public Server(InetAddress ip, int port, int backlogSize) {
+	public Server(InetAddress ip, int port) {
+		this(ip, port, DEFAULT_BACKLOG_SIZE, DEFAULT_MINIMUM_POOL_SIZE, DEFAULT_MAXIMUM_POOL_SIZE, DEFAULT_POLL_THREAD_ALIVE_TIME);
+	}
+
+	public Server(InetAddress ip, int port, int backlogSize, int minimumPoolSize, int maximumPoolSize, long poolThreadAliveTime) {
 		this.ip = ip;
 		this.port = port;
 		this.backlogSize = backlogSize;
+		this.minimumPoolSize = minimumPoolSize;
+		this.maximumPoolSize = maximumPoolSize;
+		this.poolThreadAliveTime = poolThreadAliveTime;
 		this.handlers = new ConcurrentHashMap<Class<? extends Request>, RequestHandler>();
 		this.nodes = new ConcurrentHashMap<Identifier, Node>();
 		this.setName("Server-" + SERVER_COUNTER++);
@@ -68,16 +81,17 @@ public class Server extends Thread implements Runnable {
 		running = true;
 
 		logger.info("Starting server on adddress " + ip + ":" + port);
-		
+
 		addShutdownHook();
-		
-		createWorkerPool();
+
+		createWorkerThreadPool();
 
 		try {
 			logger.debug("Opening socket on address " + ip + ":" + port + " with a message backlog of size " + backlogSize);
 			serverSocket = new ServerSocket(port, backlogSize, ip);
-		
-			while(hasToRun) {
+
+			logger.info("Server started, waiting for connections");
+			while (hasToRun) {
 				try {
 					Socket socket = serverSocket.accept();
 					logger.info("Incoming connection from " + socket.getInetAddress() + ":" + socket.getPort());
@@ -85,19 +99,19 @@ public class Server extends Thread implements Runnable {
 					logger.debug("Submiting request to a new worker in the pool (active/size: " + workerThreadPool.getActiveCount() + "/" + workerThreadPool.getPoolSize() + ")");
 					this.workerThreadPool.submit(worker);
 				} catch (IOException e) {
-					//when the server is shutting down a SocketException is generated as the socket is closed
-					if(!(e instanceof SocketException && e.getMessage().equals("Socket closed") && !hasToRun))
+					// when the server is shutting down a SocketException is generated as the socket is closed
+					if (!(e instanceof SocketException && e.getMessage().equals("Socket closed") && !hasToRun))
 						logger.error("IOException occured while trying to accept new connections", e);
 				}
 			}
-		
+
 		} catch (IOException e) {
 			logger.fatal("Could not create main server socket!", e);
 		}
-		
+
 		running = false;
 	}
-	
+
 	class Worker implements Runnable {
 
 		private Map<Class<? extends Request>, RequestHandler> handlersMap;
@@ -121,16 +135,16 @@ public class Server extends Thread implements Runnable {
 					logger.error("IOException occured while trying to open the socket inputStream");
 					throw e;
 				}
-				
+
 				try {
 					outStream = new ObjectOutputStream(socket.getOutputStream());
 				} catch (IOException e) {
 					logger.error("IOException occured while trying to open the socket outputStream");
 					throw e;
 				}
-				
+
 				Object readObj = null;
-				
+
 				try {
 					readObj = inStream.readObject();
 				} catch (IOException e) {
@@ -140,21 +154,21 @@ public class Server extends Thread implements Runnable {
 					logger.error("ClassNotFoundException occured while trying to read object from the socket");
 					throw e;
 				}
-				
-				if(readObj instanceof Request) {
+
+				if (readObj instanceof Request) {
 					Request request = (Request) readObj;
-					
+
 					logger.debug("Received request: " + request);
 
 					Contact contact = request.getDestination();
 					Node node = nodes.get(contact.asIdentifier());
-					if(node == null)
+					if (node == null)
 						throw new IllegalArgumentException("Received request is intended for a node with id " + contact.asIdentifier() + " that is not managed by this server");
 
 					RequestHandler handler = handlersMap.get(request.getClass());
-					if(handler == null)
+					if (handler == null)
 						throw new IllegalArgumentException("Received request cannot be handled by this server as no handler was found for type " + request.getClass());
-					
+
 					try {
 						try {
 							Response response = handler.handle(node, request);
@@ -168,11 +182,11 @@ public class Server extends Thread implements Runnable {
 						logger.error("IOException occured while trying to write the response object back to the client");
 						throw e;
 					}
-					
+
 				} else {
 					logger.warn("Object read from the socket is of an unsupported type (not instance of " + Request.class + "): " + readObj.getClass());
 				}
-					
+
 			} catch (Exception e) {
 				logger.error(null, e);
 			} finally {
@@ -208,10 +222,10 @@ public class Server extends Thread implements Runnable {
 	}
 
 	public void shutdown() {
-		if(running) {
+		if (running) {
 			logger.info("Shutting down server " + this.getName());
 			this.hasToRun = false;
-			if(this.serverSocket != null) {
+			if (this.serverSocket != null) {
 				try {
 					this.serverSocket.close();
 				} catch (IOException e) {
@@ -221,7 +235,7 @@ public class Server extends Thread implements Runnable {
 			logger.debug("Server " + this.getName() + " successfully shutted down");
 		}
 	}
-	
+
 	private void addShutdownHook() {
 		logger.debug("Adding server shutdown hook");
 		final Server thisRef = this;
@@ -231,12 +245,12 @@ public class Server extends Thread implements Runnable {
 			}
 		});
 	}
-	
-	private void createWorkerPool() {
-		logger.debug("Creating Handler thread pool");
-		workerThreadPool = new ThreadPoolExecutor(1, 50, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
+
+	private void createWorkerThreadPool() {
+		logger.debug("Creating a worker thread pool with size ranging from " + minimumPoolSize + " to " + maximumPoolSize + " and with a thread alive timeout of " + poolThreadAliveTime + "ms");
+		workerThreadPool = new ThreadPoolExecutor(minimumPoolSize, maximumPoolSize, poolThreadAliveTime, TimeUnit.MILLISECONDS, new SynchronousQueue<Runnable>());
 	}
-	
+
 	public <T extends Request> void addHandler(Class<T> requestClass, RequestHandler<T, ? extends Response> handler) {
 		this.handlers.put(requestClass, handler);
 	}
@@ -248,7 +262,7 @@ public class Server extends Thread implements Runnable {
 	public Collection<Class<? extends Request>> getHandledTypes() {
 		return new ArrayList<Class<? extends Request>>(handlers.keySet());
 	}
-	
+
 	public void addNode(Node node) {
 		this.nodes.put(node.asIdentifier(), node);
 	}
@@ -260,19 +274,66 @@ public class Server extends Thread implements Runnable {
 	public Collection<Node> getNodes() {
 		return new ArrayList<Node>(nodes.values());
 	}
-	
+
 	public InetAddress getIp() {
 		return ip;
 	}
-	
+
 	public int getPort() {
 		return port;
 	}
-	
+
+	public int getBacklogSize() {
+		return backlogSize;
+	}
+
+	public void setBacklogSize(int backlogSize) {
+		this.backlogSize = backlogSize;
+	}
+
+	public int getMinimumPoolSize() {
+		return minimumPoolSize;
+	}
+
+	public void setMinimumPoolSize(int minimumPoolSize) {
+		this.minimumPoolSize = minimumPoolSize;
+	}
+
+	public int getMaximumPoolSize() {
+		return maximumPoolSize;
+	}
+
+	public void setMaximumPoolSize(int maximumPoolSize) {
+		this.maximumPoolSize = maximumPoolSize;
+	}
+
+	public long getPoolThreadAliveTime() {
+		return poolThreadAliveTime;
+	}
+
+	public void setPoolThreadAliveTime(long poolThreadAliveTime) {
+		this.poolThreadAliveTime = poolThreadAliveTime;
+	}
+
+	public ThreadPoolExecutor getWorkerThreadPool() {
+		return workerThreadPool;
+	}
+
+	public boolean isRunning() {
+		return running;
+	}
+
+	public void setIp(InetAddress ip) {
+		this.ip = ip;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
 	public static void main(String[] args) throws Exception {
-		Server server = new Server(Inet4Address.getByName("localhost"), 50000);
+		Server server = new Server(50000);
 		server.start();
 	}
 
 }
-
