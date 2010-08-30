20827c1b85de20eb2b1d161295934da1544dfb1c
Bruno Polaco Penteado <polaco@gmail.com>
2010-08-30 16:45:24 -0300

    Added toString implementation + specific clonning method

--- src/main/java/com/github/bcap/dht/node/Identifier.java
+++ src/main/java/com/github/bcap/dht/node/Identifier.java
@@ -8,14 +8,15 @@ public class Identifier implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
-	public static final int LENGTH = 128;
+	public static final int LENGTH = 160;
+	private static final String ID_STRING_FORMAT = "%0" + LENGTH / 16 + "x";
 	
 	private BigInteger value;
 
 	protected Identifier() {}
 	
 	public Identifier(BigInteger value) {
-		this.value = value;
+		this.value = new BigInteger(1, value.toByteArray());
 	}
 	
 	public BigInteger getValue() {
@@ -24,6 +25,10 @@ public class Identifier implements Serializable {
 	
 	public BigInteger getDistance(Identifier anotherId) {
 		return this.value.xor(anotherId.value);
+	}
+	
+	public Identifier asIdentifier() {
+		return new Identifier(this.getValue());
 	}
 	
 	public <T extends Identifier> T getClosest(Collection<T> identifiers) {
@@ -55,5 +60,7 @@ public class Identifier implements Serializable {
 		}
 	}
 	
-	
+	public String toString() {
+		return String.format(ID_STRING_FORMAT, value).toUpperCase();
+	}
 }
