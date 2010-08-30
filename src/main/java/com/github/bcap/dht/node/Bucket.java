efa5b963028c0daadcfe369ccd3b31df08498422
Bruno Polaco Penteado <polaco@gmail.com>
2010-08-30 19:38:13 -0300

    Better equals implementation and format code

--- src/main/java/com/github/bcap/dht/node/Bucket.java
+++ src/main/java/com/github/bcap/dht/node/Bucket.java
@@ -12,36 +12,65 @@ public class Bucket extends Identifier implements Serializable, Iterable<Contact
 	private static final long serialVersionUID = 1L;
 
 	public static final int MAX_SIZE = 20;
-	
+
 	private SortedSet<Contact> contacts = new TreeSet<Contact>(new AliveTimeComparator());
-	
-	protected Bucket() {}
-	
+
+	protected Bucket() {
+	}
+
 	public Bucket(BigInteger id) {
 		super(id);
 	}
-	
+
 	public int size() {
 		return contacts.size();
 	}
-	
+
 	public void addContact(Contact contact) throws IllegalStateException {
-		if(!contacts.contains(contact) && this.size() >= MAX_SIZE)
+		if (!contacts.contains(contact) && this.size() >= MAX_SIZE)
 			throw new IllegalStateException("Cannot add, bucket at max capacity of " + MAX_SIZE);
 		else
 			contacts.add(contact);
 	}
-	
+
 	public boolean removeContact(Contact contact) {
 		return this.contacts.remove(contact);
 	}
-	
+
 	public Iterator<Contact> iterator() {
 		return contacts.iterator();
 	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bucket that = (Bucket) obj;
+		if (contacts == null) {
+			if (that.contacts != null)
+				return false;
+		} else {
+			if (this.contacts.size() != that.contacts.size()) {
+				return false;
+			} else {
+				Iterator<Contact> thisIt = this.contacts.iterator();
+				Iterator<Contact> thatIt = that.contacts.iterator();
+				while (thisIt.hasNext()) {
+					if (!thisIt.next().equals(thatIt.next()))
+						return false;
+				}
+			}
+		}
+		return true;
+	}
 }
 
-class AliveTimeComparator implements Comparator<Contact> {
+class AliveTimeComparator implements Comparator<Contact>, Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	public int compare(Contact contact1, Contact contact2) {
 		return contact1.getLastAliveDate().compareTo(contact2.getLastAliveDate());
--- src/main/java/com/github/bcap/dht/node/Identifier.java
+++ src/main/java/com/github/bcap/dht/node/Identifier.java
@@ -5,39 +5,40 @@ import java.math.BigInteger;
 import java.util.Collection;
 
 public class Identifier implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
 
 	public static final int LENGTH = 160;
 	private static final String ID_STRING_FORMAT = "%0" + LENGTH / 16 + "x";
-	
+
 	private BigInteger value;
 
-	protected Identifier() {}
-	
+	protected Identifier() {
+	}
+
 	public Identifier(BigInteger value) {
 		this.value = new BigInteger(1, value.toByteArray());
 	}
-	
+
 	public BigInteger getValue() {
 		return this.value;
 	}
-	
+
 	public BigInteger getDistance(Identifier anotherId) {
 		return this.value.xor(anotherId.value);
 	}
-	
+
 	public Identifier asIdentifier() {
 		return new Identifier(this.getValue());
 	}
-	
+
 	public <T extends Identifier> T getClosest(Collection<T> identifiers) {
 		T result = null;
 		BigInteger lesserDistance = null;
-		if(identifiers != null) {
+		if (identifiers != null) {
 			for (T node : identifiers) {
 				BigInteger distance = this.getDistance(node);
-				if(result == null || distance.compareTo(lesserDistance) < 0) {
+				if (result == null || distance.compareTo(lesserDistance) < 0) {
 					result = node;
 					lesserDistance = distance;
 				}
@@ -45,21 +46,27 @@ public class Identifier implements Serializable {
 		}
 		return result;
 	}
-	
-	public boolean equals(Object obj) {
-		if(obj instanceof Identifier) {
-			Identifier that = (Identifier) obj;
-			if(that == null)
-				return false;
-			else if (this == that)
-				return true;
-			else 
-				return this.value.equals(that.value);
-		} else {
-			return false;
-		}
+
+	public int hashCode() {
+		return value != null ? value.hashCode() : super.hashCode();
 	}
-	
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Identifier other = (Identifier) obj;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
 	public String toString() {
 		return String.format(ID_STRING_FORMAT, value).toUpperCase();
 	}
