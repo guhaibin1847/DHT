392eaaefd50c4f378c573114614a20f00935d2df
Bruno Polaco Penteado <polaco@gmail.com>
2010-09-03 14:31:13 -0300

    StoreRequestHandler basic implementation (no datasource error control)

--- core/src/main/java/com/github/bcap/dht/server/handler/RequestHandler.java
+++ core/src/main/java/com/github/bcap/dht/server/handler/RequestHandler.java
@@ -8,16 +8,16 @@ import com.github.bcap.dht.node.Contact;
 import com.github.bcap.dht.node.Node;
 import com.github.bcap.dht.server.Server;
 
-public abstract class RequestHandler <Req extends Request, Resp extends Response> {
+public abstract class RequestHandler {
 	
-	public Resp handle(Server server, Req request) throws RequestHandlerException {
+	public Response handle(Server server, Request request) throws RequestHandlerException {
 		Contact destination = request.getDestination();
 		Node node = server.getNode(destination);
 
 		if (node == null)
 			throw new RequestHandlerException("Received request is intended for a node with id " + destination.asIdentifier() + " that is not managed by this server");
 
-		Resp response = this.handleImpl(node, request);
+		Response response = this.handleImpl(node, request);
 		
 		response.setSource(new Contact(node.getValue(), server.getIp(), server.getPort()));
 		response.setDestination(request.getSource());
@@ -28,5 +28,5 @@ public abstract class RequestHandler <Req extends Request, Resp extends Response
 		return response;
 	}
 	
-	public abstract Resp handleImpl(Node node, Req request) throws RequestHandlerException;
+	public abstract Response handleImpl(Node node, Request request) throws RequestHandlerException;
 }
--- core/src/main/java/com/github/bcap/dht/server/handler/StoreRequestHandler.java
+++ core/src/main/java/com/github/bcap/dht/server/handler/StoreRequestHandler.java
@@ -1,15 +1,21 @@
 package com.github.bcap.dht.server.handler;
 
+import com.github.bcap.dht.message.request.Request;
 import com.github.bcap.dht.message.request.StoreRequest;
+import com.github.bcap.dht.message.response.Response;
 import com.github.bcap.dht.message.response.StoreResponse;
+import com.github.bcap.dht.node.Identifier;
 import com.github.bcap.dht.node.Node;
 
-public class StoreRequestHandler extends RequestHandler<StoreRequest, StoreResponse> {
+public class StoreRequestHandler extends RequestHandler {
 
 	@Override
-	public StoreResponse handleImpl(Node node, StoreRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+	public Response handleImpl(Node node, Request request) {
+		StoreRequest store = (StoreRequest) request;
+		Identifier key = store.getIdentifier();
+		byte[] data = store.getData();
+		node.getDataStorage().put(key, data);
+		return new StoreResponse();
 	}
 
 }
