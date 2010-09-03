a2d3c18a48e89f77012555fd808e023a129e8edb
Bruno Polaco Penteado <polaco@gmail.com>
2010-09-03 11:56:44 -0300

    Fixing constructors

--- core/src/main/java/com/github/bcap/dht/message/request/FindNodeRequest.java
+++ core/src/main/java/com/github/bcap/dht/message/request/FindNodeRequest.java
@@ -2,13 +2,11 @@ package com.github.bcap.dht.message.request;
 
 import java.io.Serializable;
 
-import com.github.bcap.dht.node.Contact;
-
 public class FindNodeRequest extends IdentifierRequest implements Serializable {
 
 	private static final long serialVersionUID = IdentifierRequest.serialVersionUID + 1L;
 
-	public FindNodeRequest(Contact from, Contact to) {
+	public FindNodeRequest() {
 		super();
 	}
 
