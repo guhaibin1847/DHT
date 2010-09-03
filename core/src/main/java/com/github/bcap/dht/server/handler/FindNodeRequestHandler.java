2e53da15df936e633c8d82d0af38e3b6088d75dd
Bruno Polaco Penteado <polaco@gmail.com>
2010-09-03 15:23:55 -0300

    FindNodeRequestHandler implementation

--- core/src/main/java/com/github/bcap/dht/server/handler/FindNodeRequestHandler.java
+++ core/src/main/java/com/github/bcap/dht/server/handler/FindNodeRequestHandler.java
@@ -1,16 +1,41 @@
 package com.github.bcap.dht.server.handler;
 
-import com.github.bcap.dht.message.request.FindNodeRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.bcap.dht.message.request.IdentifierRequest;
+import com.github.bcap.dht.message.request.Request;
 import com.github.bcap.dht.message.response.FindNodeResponse;
+import com.github.bcap.dht.message.response.Response;
+import com.github.bcap.dht.node.Bucket;
+import com.github.bcap.dht.node.Contact;
+import com.github.bcap.dht.node.Identifier;
 import com.github.bcap.dht.node.Node;
 
-public class FindNodeRequestHandler extends RequestHandler<FindNodeRequest, FindNodeResponse> {
+public class FindNodeRequestHandler extends RequestHandler {
 
-	@Override
-	public FindNodeResponse handleImpl(Node node, FindNodeRequest request) {
-		// TODO Auto-generated method stub
-		return null;
+	public Response handleImpl(Node node, Request request) {
+		IdentifierRequest idReq = (IdentifierRequest) request;
+		int nodeIndex = node.getBucketIndex(node);
+		int bucketIndex = node.getBucketIndex(idReq.getIdentifier());
+		int direction = nodeIndex > bucketIndex ? 1 : -1;
+		
+		FindNodeResponse response = new FindNodeResponse();
+		
+		List<Contact> contacts = new ArrayList<Contact>();
+		
+		for(int visitedBuckets = 0; contacts.size() < Bucket.MAX_SIZE && visitedBuckets < Identifier.LENGTH; visitedBuckets++) {
+			Bucket bucket = node.getBucket(bucketIndex);
+			contacts.addAll(bucket.getContacts());
+			bucketIndex = circularMod(bucketIndex + direction, Identifier.LENGTH);
+		}
+		
+		response.setContacts(contacts);
+		
+		return response;
 	}
-
-
+	
+	private int circularMod(int x, int y) {
+		return ((x % y) + y ) % y;
+	}
 }
